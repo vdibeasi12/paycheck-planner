77f466c16872cbf094ea421b3ae058e048f7de96
vdibeasi12 <vincentdibeasi@gmail.com>
2026-08-12 20:55:35 -0500

    Switch bank connect to Balance product instead of Auth

--- app/api/plaid/link-token/route.ts
+++ app/api/plaid/link-token/route.ts
@@ -76,7 +76,8 @@ export async function POST(req: Request) {
     if (accessToken) {
       params.access_token = accessToken
     } else {
-      params.products = [Products.Liabilities]
+      params.products = [Products.Balance]
+      params.additional_consented_products = [Products.Liabilities]
     }
 
     const res = await plaid.linkTokenCreate(params as any)
